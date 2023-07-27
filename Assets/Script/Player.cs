144c65ceb5be0f4307320a5701c21ce9c36b32a3
허민구 <huccce@gmail.com>
2023-07-27 20:53:10 +0900

    플레이어 점프
    플레이어 점프 구현
    플레이어 슈팅 구현 중

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 0.6f;
+    public float jumpForce = 0.5f;
+    public GameObject shotObject = null;
+    public Transform shotPosition = null;
+    private bool jumping = false;
+
 
     // Start is called before the first frame update
     private void Start()
@@ -15,6 +20,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+
+        // ¿Ãµø
         float h = Input.GetAxis("Horizontal");
         if (h == 0)
         {
@@ -32,6 +39,32 @@ public class Player : MonoBehaviour
             GetComponent<SpriteRenderer>().flipX = true;
             Move(h);
         }
+
+
+        // ¡°«¡
+        if(jumping == false)
+        {
+            bool jump = Input.GetButtonDown("Jump");
+
+            if (jump == true)
+            {
+                Vector2 jumpVector = new Vector2();
+                jumpVector.y = jumpForce;
+
+                GetComponent<Animator>().SetBool("isRun", false);
+                GetComponent<Animator>().SetBool("isJump", true);
+                GetComponent<Rigidbody2D>().AddForce(jumpVector);
+                jumping = true;
+            }
+        }
+
+        // √—ΩÓ±‚
+        bool shot = Input.GetButtonDown("Fire1");
+        if(shot == true)
+        {
+            GameObject.Instantiate(shotObject, shotPosition.position, shotPosition.rotation);
+        }
+
     }
 
     private void Move(float h)
@@ -40,5 +73,12 @@ public class Player : MonoBehaviour
         Vector3 pos = new Vector3();
         pos.x = h * moveSpeed * Time.deltaTime;
         transform.Translate(pos);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumping = false;
+        GetComponent<Animator>().SetBool("isRun", false);
+        GetComponent<Animator>().SetBool("isJump", false);
     }
 }
